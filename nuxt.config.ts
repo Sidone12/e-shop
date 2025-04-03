6077581990baeddd610d8f281a463dda482526a7
Oleksii <sidone666@gmail.com>
2025-04-03 21:57:00 +0300

    fix prisma connection and seed items

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    reddirect: false,
   },
   app: {
     head: {
---
+++ prisma/seed.js
@@ -0,0 +1,27 @@
+import {PrismaClient} from '@prisma/client';
+import {$fetch} from 'ofetch';
+const prisma = new PrismaClient();
+
+const productsURL = 'https://fakestoreapi.com/products';
+const products = await $fetch(productsURL);
+
+const seedProducts = async () => {
+  try {
+    products.forEach(async product => {
+      await prisma.products.create({
+        data: {
+          title: product.title,
+          description: product.description,
+          image: product.image,
+          category: product.category,
+          price: product.price,
+        },
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+seedProducts();
