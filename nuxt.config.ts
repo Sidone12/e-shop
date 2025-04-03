2a3e195b6032e80bc6e3e93b8c4a256d755a4439
Oleksii <sidone666@gmail.com>
2025-04-03 21:28:31 +0300

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: {enabled: true},
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/supabase', 'prisma'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    reddirect: false,
+  },
+  app: {
+    head: {
+      title: 'E-shop',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css',
+        },
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js',
+          defer: true,
+        },
+      ],
+    },
+  },
+});
